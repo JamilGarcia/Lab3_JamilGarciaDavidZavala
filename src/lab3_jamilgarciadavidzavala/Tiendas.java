901ddf2e794c5b046626ee046331408ea6c283bf
JamilGarcia <you@example.com>
2020-05-15 14:16:00 -0600

    Clases creadas

---
+++ src/lab3_jamilgarciadavidzavala/Tiendas.java
@@ -0,0 +1,43 @@
+package lab3_jamilgarciadavidzavala;
+
+import java.util.ArrayList;
+
+public class Tiendas extends Locales {
+    
+    private ArrayList <Ropa> T_Ropa; 
+    private ArrayList <Juguetes> T_Juguetes;
+
+    public Tiendas() {
+        super();
+    }
+
+    public Tiendas(ArrayList<Ropa> T_Ropa, ArrayList<Juguetes> T_Juguetes, String nombre, ArrayList<Empleados> empleados, ArrayList<lab3_jamilgarciadavidzavala.Productos> Productos, String Gerente) {
+        super(nombre, empleados, Productos, Gerente);
+        this.T_Ropa = T_Ropa;
+        this.T_Juguetes = T_Juguetes;
+    }
+
+    public ArrayList<Ropa> getT_Ropa() {
+        return T_Ropa;
+    }
+
+    public void setT_Ropa(ArrayList<Ropa> T_Ropa) {
+        this.T_Ropa = T_Ropa;
+    }
+
+    public ArrayList<Juguetes> getT_Juguetes() {
+        return T_Juguetes;
+    }
+
+    public void setT_Juguetes(ArrayList<Juguetes> T_Juguetes) {
+        this.T_Juguetes = T_Juguetes;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Tiendas{" + "T_Ropa=" + T_Ropa + ", T_Juguetes=" + T_Juguetes + '}';
+    }
+    
+    
+    
+}
