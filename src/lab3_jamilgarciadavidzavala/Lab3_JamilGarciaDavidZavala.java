5c6c3875ff83225e38a89652785e58a94bf73983
DAZA <david.j.2676@gmail.com>
2020-05-15 14:10:27 -0600

    prueba

--- src/lab3_jamilgarciadavidzavala/Lab3_JamilGarciaDavidZavala.java
+++ src/lab3_jamilgarciadavidzavala/Lab3_JamilGarciaDavidZavala.java
@@ -3,7 +3,9 @@ package lab3_jamilgarciadavidzavala;
 public class Lab3_JamilGarciaDavidZavala {
 
     public static void main(String[] args) {
-        
+        for (int i = 0; i < 100000; i++) {
+            System.out.println("Jamil se la come");
+        }
     }
     
 }
